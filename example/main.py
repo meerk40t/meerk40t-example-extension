3346655e1d402ce08355865fe70589902327e6e7
Tatarize <tatarize@gmail.com>
2022-10-02 22:46:19 -0700

    More examples, more explanations

--- example/main.py
+++ example/main.py
@@ -66,27 +66,23 @@ def service_plugin(service, lifecycle):
         print(f"A lihuiyu device was shutdown: {service.name}")
 
 
-def plugin(kernel, lifecycle):
-    """
-    This is the kernel level plugin registration.
+def invalidating_plugin(kernel, lifecycle):
+    if lifecycle == "precli":
+        kernel.register("invalidating_plugin_existed", True)
+    if lifecycle == "invalidate":
+        return True
 
-    :param kernel:
-    :param lifecycle:
-    :return:
+
+def simple_plugin(kernel, lifecycle):
     """
-    if lifecycle == "plugins":
-        return [service_plugin, module_plugin]
-    if lifecycle == 'preregister':
-        """
-        During the pre-register phase the module wxMeerK40t is registered and opened for the gui.
-        """
-        pass
-    if lifecycle == 'register':
-        """
-        Register our various processes. These should modify the registered values within meerk40t. This stage is
-        used for general purpose value registrations.
-        """
-        # Example of writing an extension to the console commands. Type: "example" in console.
+    Simple plugin. Catches the lifecycle it needs registers some values.
+
+    @param kernel:
+    @param lifecycle:
+    @return:
+    """
+    if lifecycle == "register":
+        # Example command extension to the console commands. Type: "example" in console, and this code will work.
         @kernel.console_command('example', help="Says Hello World.")
         def example_cmd(command, channel, _, **kwargs):
             """
@@ -95,32 +91,103 @@ def plugin(kernel, lifecycle):
             """
             channel(_('Hello World'))
 
-    if lifecycle == 'configure':
+        # Example tree extension for Cut and Engrave nodes to call our Hello World function.
+        @kernel.elements.tree_operation("Hello World", node_type=("op cut", "op engrave"), help="calls `example` code.")
+        def unique_tree_name_example(node, **kwargs):
+            """
+            This adds this command to the right-click tree. Any cut or engrave node type will include "Hello World".
+            :param node:
+            :param kwargs:
+            :return:
+            """
+            kernel.console("example\n")
+
+
+def plugin(kernel, lifecycle):
+    """
+    This is our main plugin. It provides examples of every lifecycle event and what they do and are used for. Many of
+    these events are simply to make sure some module events occur after or before other module events. The lifecycles
+    also permit listeners to attach and detach during the lifecycle of a module, and insures everything can interact
+    smoothly.
+
+    :param kernel:
+    :param lifecycle:
+    :return:
+    """
+    print(f"Kernel plugin calling lifecycle: {lifecycle}")
+    if lifecycle == "plugins":
         """
-        Stage between registration and before the boot stages.
+        All plugins including ones added with this call are added to the kernel. A list of additions plugins will add
+        those to the list of plugins.
+        """
+        return [service_plugin, module_plugin, invalidating_plugin, simple_plugin]
+    if lifecycle == "service":
+        """
+        Responding to this with a service provider makes this plugin a service plugin.
+        
+        Note: Normally we ignore this lifecycle.
+        """
+        return None  # This is not a service plugin, check service_plugin for an example of that.
+    if lifecycle == "module":
+        """
+        Responding to a registered module provider makes this plugin a module plugin.
+        
+        Note: Normally we ignore this lifecycle.
+        """
+        return None  # This is not a module plugin, check module_plugin for an example of this.
+    if lifecycle == "precli":
+        """
+        This lifecycle occurs before the command line options are processed. Anything part of the main CLI is processed
+        after this.
+        """
+    if lifecycle == "cli":
+        """
+        This life cycle is intended to process command line information. It is sometimes used to register features or
+        other flags that could be used during the invalidate.
+        """
+        if kernel.lookup("invalidating_plugin_existed"):
+            print("Our invalidating plugin existed and put this here.")
+    if lifecycle == "invalidate":
+        """
+        Invalidate is called with a "True" response if this plugin isn't valid or cannot otherwise execute. This is
+        often useful if a plugin is only valid for a particular OS. For example `winsleep` serve no purpose for other
+        operating systems, so it invalidates itself.
+        """
+        return False  # We are valid.
+
+    if lifecycle == 'preregister':
+        """
+        During the pre-register phase the module wxMeerK40t is registered and opened in gui mode.
         """
         pass
-    elif lifecycle == 'preboot':
+    if lifecycle == 'register':
         """
-        Preboot is usually where device services are started. Since many booting elements need to the devices to exist
-        services should be launched at this stage and prior to the boot.
+        Register our various processes. These should modify the registered values within meerk40t. This stage is
+        used for general purpose lookup registrations.
+        """
+        # See simple plugin for examples of registered objects.
+        pass
+
+    if lifecycle == 'configure':
+        """
+        Configure is a preboot stage where everything is registered but elements are not yet booted.
         """
         pass
     elif lifecycle == 'boot':
         """
         Start all services.
         
-        Register any scheduled tasks or threads that need to be running for our plugin to work.
-        Register various choices within services which should all be started. 
+        The kernel strictly registers the lookup_listeners and signal_listeners during this stage. This permits modules
+        and services to listen for signals and lookup changes during the active phases of their lifecycles.  
         """
         pass
     elif lifecycle == 'postboot':
         """
-        Registers some additional choices such as some general preferences. 
+        Registers some additional choices such as some general preferences.
         """
     elif lifecycle == 'prestart':
         """
-        CLI specified input file is loading during the pre-start phase
+        CLI specified input file is loading during the pre-start phase.
         """
         pass
     elif lifecycle == 'start':
@@ -130,7 +197,7 @@ def plugin(kernel, lifecycle):
         pass
     elif lifecycle == 'poststart':
         """
-        CLI specified output file is written during the poststart phase
+        Nothing happens.
         """
         pass
     elif lifecycle == 'ready':
@@ -153,7 +220,14 @@ def plugin(kernel, lifecycle):
         This is the start of the gui and will capture the default thread as gui thread. If we are writing a new gui
         system and we need this thread to do our work. It should be captured here. This is the main work of the program.
         
-        You cannot ensure that more than one plugin can catch the mainloop. 
+        You cannot ensure that more than one plugin can catch the mainloop. Capture of the mainloop happens for the
+        duration of the gui app, if one exists. 
+        """
+        pass
+    elif lifecycle == 'postmain':
+        """
+        Everything that was to grab the mainloop thread has finished. We are fully booted. However in most cases since
+        the gui has been killed, the kernel has likely been told to shutdown too and will end shortly.
         """
         pass
     elif lifecycle == 'preshutdown':
@@ -161,6 +235,7 @@ def plugin(kernel, lifecycle):
         Preshutdown saves the current activated device to the kernel.root to ensure it has the correct last value.
         """
         pass
+
     elif lifecycle == 'shutdown':
         """
         Meerk40t's closing down. Our plugin should adjust accordingly. All registered meerk40t processes will be stopped
